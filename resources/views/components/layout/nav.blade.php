5d446328d20e6bd160c124abfc70e75fc37871ca
r0302772 <dennis.nijs@hotmail.com>
2024-02-01 12:21:29 +0100

    Child components

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,5 @@
+<nav class="container mx-auto p-4 flex justify-between items-center">
+    <a href="{{ route('home') }}" class="underline">Home</a>
+    <a href="{{ route('contact') }}" class="underline">Contact</a>
+    <a href="{{ route('admin.records') }}" class="underline">Records</a>
+</nav>
