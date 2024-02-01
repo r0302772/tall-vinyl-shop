20e8e636fbbd0f2efdbb7e1eeb2ab1f4092f53db
r0302772 <dennis.nijs@hotmail.com>
2024-02-01 12:15:31 +0100

    Link a page to the layout component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,12 @@
-<h1>The Vinyl Shop</h1>
+<x-vinylshop-layout>
+    <x-slot name="description">New description</x-slot>
+    <x-slot name="title">Welcome to the Vinyl Shop</x-slot>
 
-<p>Welcome to the website of The Vinyl Shop, a large online store with lots of (classic) vinyl records.</p>
+    <p>Welcome to the website of The Vinyl Shop, a large online store with lots of (classic) vinyl records.</p>
+
+    @push('script')
+        <script>
+            console.log('The Vinyl Shop JavaScript works! 🙂')
+        </script>
+    @endpush
+</x-vinylshop-layout>
