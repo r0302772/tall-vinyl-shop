de80ad2cbdd205886efece902d5784882006534b
r0302772 <dennis.nijs@hotmail.com>
2024-02-01 11:37:55 +0100

    Basic routing

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'The Vinyl Shop';
+});
+
+Route::get('contact', function () {
+    return 'Contact info';
 });
 
 Route::middleware([
