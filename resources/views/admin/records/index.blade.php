3235bf106c2ef7f310c2dc394a3d0e2ce2781e0e
r0302772 <dennis.nijs@hotmail.com>
2024-02-01 12:18:07 +0100

    Refactor all pages

--- resources/views/admin/records/index.blade.php
+++ resources/views/admin/records/index.blade.php
@@ -1,18 +1,21 @@
-<h1>Records</h1>
+<x-vinylshop-layout>
+    <x-slot name="description">Admin: Records</x-slot>
+    <x-slot name="title">Records</x-slot>
 
-<h2>Using php</h2>
-<ul>
-    <?php
-    foreach ($records as $record){
-        echo "<li> $record </li>";
-        //echo '<li>' . $record . '</li>';
-    }
-    ?>
-</ul>
+    <h2>Using php</h2>
+    <ul>
+        <?php
+        foreach ($records as $record) {
+            echo "<li> $record </li>";
+            //echo '<li>' . $record . '</li>';
+        }
+        ?>
+    </ul>
 
-<h2>Using "@-foreach" and curly braces</h2>
-<ul>
-    @foreach ($records as $record)
-        <li>{!! $record !!}</li>
-    @endforeach
-</ul>
+    <h2>Using "@-foreach" and curly braces</h2>
+    <ul>
+        @foreach ($records as $record)
+            <li>{!! $record !!}</li>
+        @endforeach
+    </ul>
+</x-vinylshop-layout>
