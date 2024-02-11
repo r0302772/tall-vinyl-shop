85fa95c47968b6fa972d94219038c7687419f99a
r0302772 <dennis.nijs@hotmail.com>
2024-02-11 18:06:40 +0100

    Reusable components
    Section component

--- resources/views/playground.blade.php
+++ resources/views/playground.blade.php
@@ -1,4 +1,20 @@
 <x-vinylshop-layout>
     <x-slot name="title">Playground</x-slot>
 
+    <h2>Sections</h2>
+    <div class="grid grid-cols-3 gap-4">
+        <x-tmk.section class="col-span-3 md:col-span-1">
+            <h3>Section 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ducimus fuga nesciunt nisi quo sequi
+                voluptas. Accusantium consequuntur officiis veritatis.</p>
+        </x-tmk.section>
+        <x-tmk.section class="col-span-3 md:col-span-1">
+            <h3>Section 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab distinctio eos ex excepturi possimus, reprehenderit vitae voluptatum. Accusamus eius eum ex, explicabo illo iste maxime odio soluta, vero voluptas, voluptate!</p>
+        </x-tmk.section>
+        <x-tmk.section class="col-span-3 md:col-span-1">
+            <h3>Section 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quasi?</p>
+        </x-tmk.section>
+    </div>
 </x-vinylshop-layout>
