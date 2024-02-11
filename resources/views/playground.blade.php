bcedb842e8bfefc1856641eef3971d90b114bc43
r0302772 <dennis.nijs@hotmail.com>
2024-02-11 18:25:57 +0100

    Reusable components
    Passing dynamic data to components

--- resources/views/playground.blade.php
+++ resources/views/playground.blade.php
@@ -127,4 +127,21 @@
         <x-tmk.logo class="w-24 fill-cyan-700"/>
         <x-tmk.logo class="w-40 fill-neutral-600 hover:w-48 hover:fill-orange-700 hover:drop-shadow-lg transition"/>
     </section>
+
+    <h2>Dynamic data</h2>
+    <section class="flex flex-col">
+        @php
+            $color = 'danger';      // $color is a dynamic value !!!
+        @endphp
+
+        <x-tmk.alert type="$color">
+            Is this a red, danger alert?<br>
+            No, <code class="px-2 text-blue-600 font-black">type="$color"</code> don't work with dynamic values.
+        </x-tmk.alert>
+
+        <x-tmk.alert :type="$color">
+            Is this a red, danger alert?<br>
+            Yes, use <code class="px-2 text-blue-600 font-black">:type="$color"</code> for dynamic values.
+        </x-tmk.alert>
+    </section>
 </x-vinylshop-layout>
