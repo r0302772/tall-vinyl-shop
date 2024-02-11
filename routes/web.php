1123bf3c2b0b2f7f4fe215b323ebe76eb18a790a
r0302772 <dennis.nijs@hotmail.com>
2024-02-11 18:35:15 +0100

    Layout component: part 2
    Navbar component

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');;
 Route::view('contact', 'contact')->name('contact');;
 Route::view('playground', 'playground')->name('playground');
+Route::view('under-construction', 'under-construction')->name('under-construction');
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::redirect('/', '/admin/records');
     Route::get('records', function () {
