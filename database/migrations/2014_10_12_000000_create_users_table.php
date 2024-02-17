06e373b445ae4dac17c117480ffeab226fb10a7a
r0302772 <dennis.nijs@hotmail.com>
2024-02-17 20:18:26 +0100

    Using Seeding for Data Population

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,44 +23,6 @@ return new class extends Migration {
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
-
-        // Insert some users (inside the up-function!)
-        DB::table('users')->insert(
-            [
-                [
-                    'name' => 'John Doe',
-                    'email' => 'john.doe@example.com',
-                    'admin' => true,
-                    'password' => Hash::make('admin1234'),
-                    'created_at' => now(),
-                    'email_verified_at' => now()
-                ],
-                [
-                    'name' => 'Jane Doe',
-                    'email' => 'jane.doe@example.com',
-                    'admin' => false,
-                    'password' => Hash::make('user1234'),
-                    'created_at' => now(),
-                    'email_verified_at' => now()
-                ]
-            ]
-        );
-
-        // Add 40 dummy users inside a loop
-        for ($i = 0; $i <= 40; $i++) {
-            // Every 6th user, $active is false (0) else true (1)
-            $active = ($i + 1) % 6 !== 0;
-            DB::table('users')->insert(
-                [
-                    'name' => "ITF User $i",
-                    'email' => "itf_user_$i@mailinator.com",
-                    'password' => Hash::make("itfuser$i"),
-                    'active' => $active,
-                    'created_at' => now(),
-                    'email_verified_at' => now()
-                ]
-            );
-        }
     }
 
     /**
--- database/migrations/2024_02_17_195150_create_genres_table.php
+++ database/migrations/2024_02_17_195150_create_genres_table.php
@@ -15,29 +15,6 @@ return new class extends Migration {
             $table->string('name')->unique();
             $table->timestamps();
         });
-
-        // Insert some genres (inside up-function, after create-method)
-        DB::table('genres')->insert(
-            [
-                ['name' => 'pop/rock', 'created_at' => now()],
-                ['name' => 'punk', 'created_at' => now()],
-                ['name' => 'industrial', 'created_at' => now()],
-                ['name' => 'hardrock', 'created_at' => now()],
-                ['name' => 'new wave', 'created_at' => now()],
-                ['name' => 'dance', 'created_at' => now()],
-                ['name' => 'reggae', 'created_at' => now()],
-                ['name' => 'jazz', 'created_at' => now()],
-                ['name' => 'dubstep', 'created_at' => now()],
-                ['name' => 'blues', 'created_at' => now()],
-                ['name' => 'indie rock', 'created_at' => now()],
-                ['name' => 'noise', 'created_at' => now()],
-                ['name' => 'electro', 'created_at' => now()],
-                ['name' => 'techno', 'created_at' => now()],
-                ['name' => 'folk', 'created_at' => now()],
-                ['name' => 'hip hop', 'created_at' => now()],
-                ['name' => 'soul', 'created_at' => now()],
-            ]
-        );
     }
 
     /**
