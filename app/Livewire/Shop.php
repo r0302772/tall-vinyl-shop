ecb11136fb77037ee74c8bfc4421d99f19ccd3cf
r0302772 <dennis.nijs@hotmail.com>
2024-02-17 22:04:22 +0100

    Shop: master section
    Pagination

--- app/Livewire/Shop.php
+++ app/Livewire/Shop.php
@@ -5,14 +5,20 @@ namespace App\Livewire;
 use App\Models\Record;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Shop extends Component
 {
+    use WithPagination;
+
+    // public properties
+    public $perPage = 6;
+
     #[Layout('layouts.vinylshop', ['title' => 'Shop', 'description' => 'Welcome to our shop'])]
     public function render()
     {
         $records = Record::orderBy('artist')
-            ->get();
+            ->paginate($this->perPage);
         return view('livewire.shop', compact('records'));
     }
 }
--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -4,6 +4,7 @@
     {{-- filter section: artist or title, genre, max price and records per page --}}
 
     {{-- master section: cards with paginationlinks --}}
+    <div class="my-4">{{ $records->links() }}</div>
     <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8 mt-8">
         @foreach ($records as $record)
         <div
@@ -38,6 +39,7 @@
         </div>
         @endforeach
     </div>
+    <div class="my-4">{{ $records->links() }}</div>
 
     {{-- Detail section --}}
     <x-tmk.livewire-log :records="$records" />
