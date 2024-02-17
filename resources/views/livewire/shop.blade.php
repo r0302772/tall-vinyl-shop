48012ba520311de510b01ce159ebc0ef41d85b2a
r0302772 <dennis.nijs@hotmail.com>
2024-02-17 22:00:41 +0100

    Shop: master section
    Update the card

--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -10,21 +10,25 @@
             wire:key="record-{{ $record->id }}"
             class="flex bg-white border border-gray-300 shadow-md rounded-lg overflow-hidden">
             <img class="w-52 h-52 border-r border-gray-300 object-cover"
-                 src="{{ asset('storage/covers/no-cover.png') }}"
-                 alt=""
-                 title="">
+                 src="{{ $record->cover }}"
+                 alt="{{ $record->title }}"
+                 title="{{ $record->title }}">
             <div class="flex-1 flex flex-col">
                 <div class="flex-1 p-4">
-                    <p class="text-lg font-medium">Artist</p>
-                    <p class="italic pb-2">Title</p>
-                    <p class="italic font-thin text-right pt-2 mb-2">Genre</p>
+                    <p class="text-lg font-medium">{{ $record->artist }}</p>
+                    <p class="italic pb-2">{{ $record->title }}</p>
+                    <p class="italic font-thin text-right pt-2 mb-2">{{ $record->genre_name }}</p>
                 </div>
                 <div class="flex justify-between border-t border-gray-300 bg-gray-100 px-4 py-2">
-                    <div>Price</div>
+                    <div>{{ $record->price_euro }}</div>
                     <div class="flex space-x-4">
-                        <button class="w-6 hover:text-red-900">
-                            <x-phosphor-shopping-bag-light/>
-                        </button>
+                        @if($record->stock > 0)
+                            <button class="w-6 hover:text-red-900">
+                                <x-phosphor-shopping-bag-light/>
+                            </button>
+                        @else
+                            <p class="font-extrabold text-red-700">SOLD OUT</p>
+                        @endif
                         <button class="w-6 hover:text-red-900">
                             <x-phosphor-music-notes-light/>
                         </button>
@@ -36,4 +40,5 @@
     </div>
 
     {{-- Detail section --}}
+    <x-tmk.livewire-log :records="$records" />
 </div>
