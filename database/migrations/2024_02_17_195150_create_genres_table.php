f872e3caf034e9c18e53f204b208af945d69165f
r0302772 <dennis.nijs@hotmail.com>
2024-02-17 20:04:15 +0100

    Add fixed data to the tables

--- database/migrations/2024_02_17_195150_create_genres_table.php
+++ database/migrations/2024_02_17_195150_create_genres_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,6 +15,29 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->timestamps();
         });
+
+        // Insert some genres (inside up-function, after create-method)
+        DB::table('genres')->insert(
+            [
+                ['name' => 'pop/rock', 'created_at' => now()],
+                ['name' => 'punk', 'created_at' => now()],
+                ['name' => 'industrial', 'created_at' => now()],
+                ['name' => 'hardrock', 'created_at' => now()],
+                ['name' => 'new wave', 'created_at' => now()],
+                ['name' => 'dance', 'created_at' => now()],
+                ['name' => 'reggae', 'created_at' => now()],
+                ['name' => 'jazz', 'created_at' => now()],
+                ['name' => 'dubstep', 'created_at' => now()],
+                ['name' => 'blues', 'created_at' => now()],
+                ['name' => 'indie rock', 'created_at' => now()],
+                ['name' => 'noise', 'created_at' => now()],
+                ['name' => 'electro', 'created_at' => now()],
+                ['name' => 'techno', 'created_at' => now()],
+                ['name' => 'folk', 'created_at' => now()],
+                ['name' => 'hip hop', 'created_at' => now()],
+                ['name' => 'soul', 'created_at' => now()],
+            ]
+        );
     }
 
     /**
