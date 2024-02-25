9deb2750959af7834115f8f5736405f4ba45167a
r0302772 <dennis.nijs@hotmail.com>
2024-02-25 18:56:59 +0100

    Logout

--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -57,7 +57,10 @@
                     <x-dropdown-link href="{{ route('profile.show') }}">Update Profile</x-dropdown-link>
                     <x-dropdown-link href="{{ route('under-construction') }}">Order history</x-dropdown-link>
                     <div class="border-t border-gray-100"></div>
-                    <x-dropdown-link href="{{ route('under-construction') }}">Logout</x-dropdown-link>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition">Logout</button>
+                    </form>
                     <div class="border-t border-gray-100"></div>
                     {{-- admins only --}}
                     <div class="block px-4 py-2 text-xs text-gray-400">Admin</div>
